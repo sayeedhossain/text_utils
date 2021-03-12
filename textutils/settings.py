1ca86c4c82dde7bcd4d9cb98bfd5b7c0287e49ce
SAYEED HOSSAIN <sayeedhatim7287@gmail.com>
2021-03-13 00:09:09 +0600

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ydgz1h&4)favzvza@sc+&o6ukpf6#&ts_q@va6+qs3pk2lo$e9'
+SECRET_KEY = 'Enter your secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
